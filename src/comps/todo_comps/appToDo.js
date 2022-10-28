2ae92f2bcea36bf298c275ea66654da73d74db50
yonatan <yonatannasimov@gmail.com>
2022-10-28 19:40:17 +0300

    ok

--- src/comps/todo_comps/appToDo.js
+++ src/comps/todo_comps/appToDo.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import TodoInput from '../../todoRedox_comps/todoInput'
-import TodoList from '../../todoRedox_comps/todoList'
+
 
 export default function AppToDo() {
   return (
     <div>
-      <TodoInput/>
-      <TodoList/>
+     
     </div>
   )
 }
