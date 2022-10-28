4d1c2d3001fa9cf5b1e501508574039ead388b24
yonatan <yonatannasimov@gmail.com>
2022-10-28 22:31:24 +0300

    ok

--- src/comps/todo_comps/todoList.js
+++ src/comps/todo_comps/todoList.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { useSelector } from "react-redux"
 
 export default function TodoList() {
+  const todos_ar = useSelector(state => state.todoSlice.todo_ar);
   return (
     <div className='mx-auto col-md-6 my-2 p-2 row'>
-      <div className='col-md-6 p-2'>
-        <div className='p-2 border shadow'>
-          <img style={{ cursor: "pointer" }} className='float-end m-1 ' src='images/trash.png' alt='delete button' />
-          <h4>Chest : push ups</h4>
-          <p className='h5 text-danger'>15 X 3</p>
-        </div>
-      </div>
+
+      {todos_ar.map(item => {
+        return (
+          <div className='col-md-6 p-2'>
+            <div key={item.id} className='p-2 border shadow'>
+              <img style={{ cursor: "pointer" }} className='float-end m-1 ' src='images/trash.png' alt='delete button' />
+              <h4>{item.muscleGroup} : {item.exerciseName}</h4>
+              <p className='h5 text-danger'>{item.numberOfRepetitions} X {item.numberOfSets}</p>
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   )
 }
