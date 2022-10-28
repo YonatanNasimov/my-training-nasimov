58cb98ddd967f71accc23f26b2a8d690a90cfa9b
yonatan <yonatannasimov@gmail.com>
2022-10-28 19:38:21 +0300

    ok

--- src/comps/appRouter.js
+++ src/comps/appRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import Counter from './counter'
 import Home from './home'
+import AppToDo from './todo_comps/appToDo'
 
 export default function AppRouter() {
     return (
@@ -12,12 +13,14 @@ export default function AppRouter() {
                     <div style={{ display: "flex", justifyItems: "center", alignItems: "center" }}>
                         <Link className='me-3' to="/">Home</Link>
                         <Link to="/counter">Counter</Link>
+                        <Link to="/todoList">Todo List</Link>
                     </div>
                 </nav>
             </header>
             <Routes>
                 <Route path='/' element={<Home />}>Home</Route>
                 <Route path='/counter' element={<Counter />}>Counter</Route>
+                <Route path='/todoList' element={<AppToDo/>}>AppToDo</Route>
             </Routes>
         </BrowserRouter>
     )
--- src/comps/todo_comps/todoInput.js
+++ src/comps/todo_comps/todoInput.js
@@ -1,7 +1,50 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 export default function TodoInput() {
+  const { register, handleSubmit, formState: { errors }, getValues } = useForm();
+
+  const onSub = (dataBody) => {
+      delete dataBody.phone2;
+      console.log(dataBody)
+  }
+
+  const nameRef = register("name", { required: true, minLength: 2 })
+  const phoneRef = register("phone", { required: true, minLength: 5 })
+  const phone2Ref = register("phone2", { required: true, validate:(val) => {
+      return val == getValues("phone")
+  } })
+  const emailRef = register("email", {
+      required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+  })
+  const colortRef = register("color", { required: true})
+
   return (
-    <div>TodoInput</div>
+      <div className='container'>
+          <h2>Form in react</h2>
+          <form onSubmit={handleSubmit(onSub)} className='col-md-6 p-2'>
+              <label>Name:</label>
+              <input {...nameRef} type="text" className='form-control' />
+              {errors.name && <div className='text-danger'>* Enter valid name: min 2 chars</div>}
+              <label>Phone:</label>
+              <input {...phoneRef} type="tel" className='form-control' />
+              {errors.phone && <div className='text-danger'>* Enter valid name: min 5 chars</div>}
+              <label>Enter Phone again:</label>
+              <input {...phone2Ref} type="tel" className='form-control' />
+              {errors.phone2 && <div className='text-danger'>* Enter the same phone!</div>}
+              <label>Email:</label>
+              <input {...emailRef} type="text" className='form-control' />
+              {errors.email && <div className='text-danger'>* Enter valid email</div>}
+              <label>Choose color</label>
+              <select {...colortRef} className='form-select m-0'>
+                  <option value="">Choose color...</option>
+                  <option value="orange">Orange</option>
+                  <option value="red">Red</option>
+                  <option value="blue">blue</option>
+              </select>
+              {errors.color && <div className='text-danger'>* you must choose a color</div>}
+              <button className='btn btn-info mt-3'>Send</button>
+          </form>
+      </div>
   )
 }
