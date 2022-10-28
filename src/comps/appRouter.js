5fa8cca8031131a63e43c73d21626d13e0159f45
yonatan <yonatannasimov@gmail.com>
2022-10-28 18:26:58 +0300

    ok

--- src/comps/appRouter.js
+++ src/comps/appRouter.js
@@ -6,15 +6,18 @@ import Home from './home'
 export default function AppRouter() {
     return (
         <BrowserRouter>
-            <header style={{minHeight:"48px",display:"flex",justifyItems:"center",alignItems:"center"}} className='container-fluid bg-dark text-white'>
-                <nav className='container'>
-                    <Link className='me-2' to="/">Home</Link>
-                    <Link to="/counter">Counter</Link>
+            <header style={{ minHeight: "48px", display: "flex", justifyItems: "center", alignItems: "center" }} className='container-fluid bg-dark text-white'>
+                <nav className='container d-flex'>
+                    <h1 className='me-5'>My Logo</h1>
+                    <div style={{ display: "flex", justifyItems: "center", alignItems: "center" }}>
+                        <Link className='me-3' to="/">Home</Link>
+                        <Link to="/counter">Counter</Link>
+                    </div>
                 </nav>
             </header>
             <Routes>
-                <Route path='/' element={<Home/>}>Home</Route>
-                <Route path='/counter' element={<Counter/>}>Counter</Route>
+                <Route path='/' element={<Home />}>Home</Route>
+                <Route path='/counter' element={<Counter />}>Counter</Route>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/comps/todo_comps/appToDo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppToDo() {
+  return (
+    <div>AppToDo</div>
+  )
+}
