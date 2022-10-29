f8f665d944d62daa5597916aeb98fe629563a766
yonatan <yonatannasimov@gmail.com>
2022-10-29 19:07:27 +0300

    ok

--- src/comps/todo_comps/todoList.js
+++ src/comps/todo_comps/todoList.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { deleteSingleItem } from '../../features/todoSlice';
 
 export default function TodoList() {
+
+  const dispatch = useDispatch();
+
   const todos_ar = useSelector(state => state.todoSlice.todo_ar);
+
   return (
     <div className='mx-auto col-md-6 my-2 p-2 row'>
 
@@ -10,7 +15,12 @@ export default function TodoList() {
         return (
           <div key={item.id} className='col-md-6 p-2'>
             <div className='p-2 border shadow h-100 overflow-hiden'>
-              <img style={{ cursor: "pointer" }} className='float-end m-1 ' src='images/trash.png' alt='delete button' />
+              <img onClick={() => {
+                
+                window.confirm("are you sure you want to delete exercise "+item.exerciseName) && dispatch(deleteSingleItem({ delId: item.id }))
+
+              }}
+                style={{ cursor: "pointer" }} className='float-end m-1 ' src='images/trash.png' alt='delete button' />
               <h4>{item.muscleGroup} : {item.exerciseName}</h4>
               <p className='h5 text-danger'>{item.numberOfRepetitions} X {item.numberOfSets}</p>
             </div>
--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,24 +1,39 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
+
+const KEY_LOCAL = "todo_local";
 
 const initState = {
-    todo_ar:[
-        {exerciseName:"push ups",numberOfSets:4,numberOfRepetitions:15,muscleGroup:"Chest",id:1},
-        {exerciseName:"push ups2",numberOfSets:3,numberOfRepetitions:12,muscleGroup:"Back",id:2}
+    todo_ar: [
+        { exerciseName: "push ups", numberOfSets: 4, numberOfRepetitions: 15, muscleGroup: "Chest", id: 1 },
+        { exerciseName: "push ups2", numberOfSets: 3, numberOfRepetitions: 12, muscleGroup: "Back", id: 2 }
     ]
 }
 
 const todoSlice = createSlice({
-    name:"todos",
-    initialState:initState,
-    reducers:{
-        addNewItem:(state,action) => {
-            state.todo_ar.push(action.payload.todoItem)
+    name: "todos",
+    initialState: initState,
+    reducers: {
+        addNewItem: (state, action) => {
+            state.todo_ar.push(action.payload.todoItem);
+            saveToLocal(state);
         },
-        resetAllItems:(state,action) => {
-            state.todo_ar = [<h2>No exercises exist</h2>];
+        resetAllItems: (state, action) => {
+            state.todo_ar = [];
+            saveToLocal(state);
+        },
+        deleteSingleItem: (state, action) => {
+            state.todo_ar = state.todo_ar.filter
+                (
+                    (item) => item.id != action.payload.delId
+                )
+            saveToLocal(state);
         }
     }
 })
 
-export const {addNewItem ,resetAllItems} = todoSlice.actions;
+const saveToLocal = (state) => {
+    localStorage.setItem(KEY_LOCAL, JSON.stringify(state))
+}
+
+export const { addNewItem, resetAllItems, deleteSingleItem } = todoSlice.actions;
 export default todoSlice.reducer;
