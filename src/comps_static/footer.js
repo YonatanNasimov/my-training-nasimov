a9aa0d89f59f10a32ade8d30d833a4397d7593f0
yonatan <yonatannasimov@gmail.com>
2022-10-29 20:37:19 +0300

    ok

---
+++ src/comps_static/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    const today = new Date();
+    const year = today.getFullYear();
+    return (
+        <footer className='bg-dark text-center' style={{ minHeight: "48px" }}>
+
+        </footer>
+    )
+}
--- src/comps_static/header.js
+++ src/comps_static/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header style={{ minHeight: "48px", display: "flex", justifyItems: "center", alignItems: "center" }} className='container-fluid bg-dark text-white'>
             <nav className='container d-flex'>
-                <h1 className='me-5'>My Logo</h1>
+                <h1 className='me-5  display-6 text-danger'>My Exercises</h1>
                 <div style={{ display: "flex", justifyItems: "center", alignItems: "center" }}>
                     <Link className='me-3' to="/">Home</Link>
                     <Link to="/todoList">Todo List</Link>
