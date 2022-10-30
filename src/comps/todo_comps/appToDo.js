e8dea47a5c370effecb05eb63a307e35b2039090
yonatan <yonatannasimov@gmail.com>
2022-10-30 13:14:58 +0200

    ok

--- src/comps/todo_comps/appToDo.js
+++ src/comps/todo_comps/appToDo.js
@@ -6,7 +6,7 @@ import "../style/home.css"
 
 export default function AppToDo() {
   return (
-    <div className='text-white myBg h-75' style={{backgroundImage:"url(images/cover.jpg)"}}>
+    <div className='text-white myBg1 h-75' style={{backgroundImage:"url(images/cover.jpg)"}}>
       <h1 className='display-4 text-center py-3'>App to do:</h1>
      <TodoInput/>
      <TodoList/>
